7f846d2faf3eb7d81725c4db08ec12d8cb521f5e
jack <1908724366@qq.com>
2023-06-09 13:09:58 +0800

    chore: 引入项目所需的依赖，设置文件夹别名，设置代理，解决跨域

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,16 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
+// 自动导入Element Plus
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// 自动导入图标
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+
+const publicDir = resolve('resources')
+const envDir = resolve('build')
 
 export default defineConfig({
   main: {
@@ -10,11 +20,58 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
+    publicDir,
+    envDir,
+    envPrefix: 'RENDERER_',
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@views': resolve('src/renderer/src/views'),
+        '@store': resolve('src/renderer/src/store'),
+        '@utils': resolve('src/renderer/src/utils'),
+        '@types': resolve('src/renderer/src/types'),
+        '@api': resolve('src/renderer/src/api'),
+        '@config': resolve('src/renderer/src/config')
       }
     },
-    plugins: [vue()]
+    server: {
+      proxy: {
+        '/api': {
+          // target: 'http://demo.open.xuexiluxian.cn',
+          target: 'http://uat.crm.xuexiluxian.cn',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    },
+    plugins: [
+      vue(),
+      AutoImport({
+        // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+        imports: ['vue'],
+
+        resolvers: [
+          // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+          ElementPlusResolver(),
+          // 自动导入图标组件
+          IconsResolver({
+            prefix: 'Icon'
+          })
+        ]
+      }),
+      Components({
+        resolvers: [
+          // 自动注册图标组件
+          IconsResolver({
+            enabledCollections: ['ep']
+          }),
+          // 自动导入 Element Plus 组件
+          ElementPlusResolver()
+        ]
+      }),
+      Icons({
+        autoInstall: true
+      })
+    ]
   }
 })
