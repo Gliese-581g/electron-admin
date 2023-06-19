0feac53fff405c975ec31b5f43b1aa38ca58b357
jack <1908724366@qq.com>
2023-06-19 13:08:46 +0800

    全局注册ep图标

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -3,7 +3,14 @@ import App from './App.vue'
 import router from './router'
 import pinia from './store'
 import './assets/css/resets.css'
+/* 注册所有图标（需要动态引入图标，目前解决方法，或者使用在线图标）
+@element-plus/icons-vue 中导入所有图标并进行全局注册 */
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.use(router)
 app.use(pinia)
 
