5b04ddd2499840ee2c566ce48097d504f7925877
jack <1908724366@qq.com>
2023-06-27 19:25:40 +0800

    fix：解决刷新时路由matched为空时报错

--- src/renderer/src/router/index.ts
+++ src/renderer/src/router/index.ts
@@ -11,7 +11,7 @@ router.beforeEach(async (to, from) => {
   const authStore = useAuthStore()
   const routesStore = useRoutesStore()
   const isAuth = authStore.isAuth
-  console.log(isAuth, to.path)
+  // console.log(isAuth, to.path)
   // 只要没登录，都跳转到登录页，如果是导航到登录页就直接放行而不跳转
   // 每次return加参数跳转都会开启一个路由守卫的生命周期钩子
   if (!isAuth && to.path !== '/login') return { path: '/login' }
@@ -22,6 +22,7 @@ router.beforeEach(async (to, from) => {
     //获取路由表
     try {
       await routesStore.getAsyncRoutes()
+      console.log(to.path)
       routesStore.routesMap.forEach((route) => {
         router.addRoute('Layout', route)
       })
@@ -29,12 +30,15 @@ router.beforeEach(async (to, from) => {
     } catch (error) {
       return
     }
-  } else return
+  } else {
+    console.log('hello')
+    return
+  }
 })
 export default router
 
 export const resetRouter = (): void => {
-  const resetWhiteNameList = ['Redirect', 'Login', 'NoFind', 'Layout']
+  const resetWhiteNameList = ['Redirect', 'Login', 'NotFound', 'Layout']
   router.getRoutes().forEach((route) => {
     const { name } = route
     if (name && !resetWhiteNameList.includes(name as string)) {
--- src/renderer/src/router/routes/index.ts
+++ src/renderer/src/router/routes/index.ts
@@ -1,5 +1,6 @@
 import Layout from '@layout/Layout.vue'
 import Login from '@views/login/Login.vue'
+import NotFound from '@views/ErrorPage/NotFound.vue'
 const routes = [
   {
     path: '/',
@@ -13,6 +14,13 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
+  },
+  /*   如果当前路由是动态追加的，beforeEach第一次执行的时候，这个路由还没追加进去，那么路由的matched为空，就会报这个警告。
+解决办法，在router/index.ts文件里，也就是初始化路由的文件里，注册好404路由，这样第一次执行beforeEach的时候匹配不到路由，就会匹配已经有的404路由 */
+  {
+    path: '/:matched(.*)*',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
