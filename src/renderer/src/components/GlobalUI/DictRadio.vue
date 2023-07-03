4fc8879b9339d029729f5c8fce0188192fa484fa
jack <1908724366@qq.com>
2023-07-03 11:42:45 +0800

    refactor：使用插件方式引入全局UI组件

---
+++ src/renderer/src/components/GlobalUI/DictRadio.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-radio v-for="item in dictOptions" :key="item.id" :label="item.v">{{ item.k }}</el-radio>
+</template>
+
+<script setup lang="ts">
+import { useDictStore } from '@renderer/store/dict'
+import { onBeforeMount, ref } from 'vue'
+const props = defineProps<{
+  dictType: string
+}>()
+const dictOptions = ref<any[]>([])
+onBeforeMount(async () => {
+  const dictStore = useDictStore()
+  dictOptions.value = await dictStore.getDictOptions(props.dictType)
+})
+</script>
+
+<style lang="scss" scoped></style>
--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -14,11 +14,8 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
-import DictTag from '@components/Dict/DictTag.vue'
-import DictOptions from '@components/Dict/DictOptions.vue'
-app.component('DictTag', DictTag)
-app.component('DictOptions', DictOptions)
-
+import GlobalUI from '@components/GlobalUI'
+app.use(GlobalUI)
 app.use(router)
 app.use(pinia)
 
